c201fbc430e9740cca01b4b64d69843eba71e03b
massu <switch153r81953@icloud.com>
2019-10-21 16:39:24 +0900

    SQLiteデータベースを追加

--- AccessTextFile.java
+++ AccessTextFile.java
@@ -1,7 +1,26 @@
 import java.util.List;
 
 class AccessTextFile {
-    
+
+    //追加メソッド
+    void addData(String targetData) {
+
+    }
+
+
+    //削除メソッド
+    void deleteData(String targetData) {
+        dataList = readData("dataset.txt");
+        List<String> deletedList;
+        for(int i = 0; i < dataList.length(); i++) {
+            if(!dataList[i].equals(targetData)) {
+                deletedList.add(dataList[i]);
+            }
+        }
+        updateData("dataset.txt", deleteList);
+    }
+
+    //読み込みメソッド
     List<String> readData(String fileName) {
         List<String> data;
         try {
@@ -17,12 +36,14 @@ class AccessTextFile {
         return data;
     }
 
-    void writeData(String fileName, String data) {
+    //書き込みメソッド
+    void updateData(String fileName, List<String> dataList, boolean appendFlag) {
         try {
             // 文字コードUTF-8を指定してPrintWriterオブジェクトを作る。
-            PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8"));
-
-            out.println(data);
+            PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(fileName, appendFlag), "UTF-8"));
+            for(int i = 0; i < dataList.size(); i++) {
+                out.println(dataList.get(i));
+            }
             out.close();
         } catch (IOException e) {
             e.printStackTrace();
