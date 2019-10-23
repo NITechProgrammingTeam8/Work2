89d2a6b9f600b99af95157de3773dfc255dac34c
massu <switch153r81953@icloud.com>
2019-10-24 00:50:27 +0900

    アーキテクチャの設計を行った

---
+++ model/Dao.java
@@ -0,0 +1,16 @@
+// 直接データベースにアクセスするクラス
+
+class Dao {
+
+    public void addData(String newData) {
+
+    }
+
+    public List<String> SearchData(String targetData) {
+
+    }
+
+    public void deleteData(String targetData) {
+
+    }
+}
--- presenter/Presenter.java
+++ presenter/Presenter.java
@@ -1,5 +1,9 @@
+// DaoやTextDAOなどのModelクラスとViewクラスの接合部
+
 class Presenter {
 
+    private Dao dao = new Dao();
+    private TextDAO textDao = new TextDAO();
 
     // GUI起動時にTextDAOに対してテキストファイルからデータベースへの読み込みを指示する
     // あおしゅー：起動時に必ずこのメソッドを呼んでください
@@ -18,18 +22,18 @@ class Presenter {
     // 新規データをデータベースに書き込むように指示する
     // あおしゅー：データ追加のアクションを受け取ったとき、このメソッドを呼んでください
     public void addData(String newData) {
-
+        dao.addData(newData);
     }
 
     // 検索データをデータベースから取得するように指示する
     // あおしゅー：データ検索のアクションを受け取ったとき、このメソッドを呼んでください
     public List<String> searchData(String targetData) {
-
+        dao.SearchData(targetData);
     }
 
     // 削除データをデータベースに削除するように支持する
     // あおしゅー：データ削除のアクションを受け取ったとき、このメソッドを呼んでください
     public void deleteData(String targetData) {
-
+        dao.deleteData(targetData);
     }
 }
